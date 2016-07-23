2a7a34ae9de25ba980b4cf77c5fdd1668caf53d6
erik_uy <erik_uy@yahoo.com>
2016-07-23 10:28:12 +1000

    Added pokemon log to file

--- pogom/models.py
+++ pogom/models.py
@@ -9,7 +9,7 @@ from base64 import b64encode
 
 from .utils import get_pokemon_name
 from .transform import transform_from_wgs_to_gcj
-from .customLog import printPokemon
+from .customLog import printPokemon, logPokemon
 
 db = SqliteDatabase('pogom.db')
 log = logging.getLogger(__name__)
@@ -87,6 +87,7 @@ def parse_map(map_dict):
                 (p['last_modified_timestamp_ms'] +
                  p['time_till_hidden_ms']) / 1000.0)
             printPokemon(p['pokemon_data']['pokemon_id'],p['latitude'],p['longitude'],d_t)
+            logPokemon(b64encode(str(p['encounter_id'])),p['spawnpoint_id'],p['pokemon_data']['pokemon_id'],p['latitude'],p['longitude'],d_t)
             pokemons[p['encounter_id']] = {
                 'encounter_id': b64encode(str(p['encounter_id'])),
                 'spawnpoint_id': p['spawnpoint_id'],
--- pogom/utils.py
+++ pogom/utils.py
@@ -67,6 +67,7 @@ def insert_mock_data():
                        longitude=locations[i][1],
                        disappear_time=disappear_time,
                        detect_time=detect_time)
+        
 
     for i in range(num_pokestop):
 
